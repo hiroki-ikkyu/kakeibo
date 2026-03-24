177ad3f084b2f3342e52ab0b0ca27d94ee6b0555
hiroki-ikkyu <124574942+hiroki-ikkyu@users.noreply.github.com>
2026-03-24 10:47:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,16 @@
-const CACHE_NAME = 'kakeibo-v3';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+const V = 'v10';
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== V).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-      return res;
+    fetch(e.request).then(r => {
+      const c = r.clone();
+      caches.open(V).then(cache => cache.put(e.request, c));
+      return r;
     }).catch(() => caches.match(e.request))
   );
 });
