336f54e4ad053b2c339811a28c032bad3263db4f
hiroki-ikkyu <124574942+hiroki-ikkyu@users.noreply.github.com>
2026-03-24 09:28:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kakeibo-v1';
+const CACHE_NAME = 'kakeibo-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -25,6 +25,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
